7f9844c7da9ccba7261afd5cf365af426fd82199
Michael Mensah <msquaremega@gmail.com>
2022-09-21 21:19:26 +0100

    add log event to node

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const logEvent = require('./utils/logEvents');
 const { v4: uuid } = require('uuid');
 const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
+const EventEmitter = require('events');
 
-console.log(uuid());
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('event', (msg) => logEvent(msg));
+
+myEmitter.emit('event', { id: uuid(), message: 'Hello World!' });
+
--- utils/logEvents.js
+++ utils/logEvents.js
@@ -5,7 +5,10 @@ const path = require('path');
 const logEvents = async (event) => {
     console.log(event);
     try {
-        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), JSON.stringify(event) + ' ');
+        if(!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'));
+        }
+        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), JSON.stringify(event) + '\n');
     } catch(err) {
         console.log(err);
     }
