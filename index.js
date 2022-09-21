216d5259c6e0f8d77bf7e01f1a67f06e8d5aa6f2
Michael Mensah <msquaremega@gmail.com>
2022-09-21 23:13:52 +0100

    add log event to node

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const logEvent = require('./utils/logEvents');
+const { logger } = require('./utils/logEvents');
 const { format } = require('date-fns');
 const express = require('express');
 const app = express();
 const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
-const EventEmitter = require('events');
+const cors = require('cors');
+// const EventEmitter = require('events');
 
-class Emitter extends EventEmitter {}
+// class Emitter extends EventEmitter {}
 
-const myEmitter = new Emitter();
+// const myEmitter = new Emitter();
 
 const port = process.env.PORT || 3500;
+
+app.use(logger);
+
+const white_list = [ 
+    'http://localhost:3500', 
+    'http://localhost:8080', 
+    'http://localhost',
+    'https://www.google.com',
+    'http://127.0.0.1:5500',
+    'http://127.0.0.1'
+]
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(white_list.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS for ' + origin));
+        }
+    },
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 
--- utils/logEvents.js
+++ utils/logEvents.js
@@ -1,17 +1,34 @@
 const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
+const { format } = require('date-fns');
 
-const logEvents = async (event) => {
-    console.log(event);
+const logEvents = async (event, logName) => {
+    // console.log(event);
     try {
         if(!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', 'eventLog.txt'), JSON.stringify(event) + '\n');
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), JSON.stringify(event) + '\n');
     } catch(err) {
         console.log(err);
     }
 }
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+    const date = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    const log = { 
+        date: date, 
+        method: req.method, 
+        url: req.url, 
+        status: res.statusCode, 
+        ip: req.ip, 
+        headerOrigin: req.headers.origin 
+    };
+    if(req.headers.origin !== undefined) {
+        logEvents(log, 'logger.txt');
+    }
+    next();
+}
+
+module.exports = { logEvents, logger };
